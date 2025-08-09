e7717f0b2289dfff71b44f0f92cda381030762bc
sandress2998 <sandress2000@mail.ru>
2025-08-09 17:39:25 +0300

    Some small fix in form text

--- src/main/java/ru/mephi/telegrambotdating_java/model/data/button/FormFillingButton.java
+++ src/main/java/ru/mephi/telegrambotdating_java/model/data/button/FormFillingButton.java
@@ -17,20 +17,35 @@ public class FormFillingButton extends AbstractInput {
         }
 
         ActivityButtonChat currentForm = repository.getByChatId(data.getChatId());
+        boolean isAllFieldFilled =  currentForm != null &&
+                currentForm.receiverTag != null &&
+                currentForm.deactivationCode != null &&
+                currentForm.address != null &&
+                currentForm.datingTime != null;
+
         return new SendMessage(data.getChatId(), String.format("""
                         Текущая анкета:
                         Получатель: %s
-                        Код деактивации: XXXX\s
+                        Код деактивации: %s
                         Адрес: %s
                         Время встречи: %s
+                        
+                        Время отправки: %s
+                        
+                        %s
 
                         Для редактирования анкеты введите данные в формате:\s
                         - <тэг получателя (без @)> ENTER
                         - <код деактивации (4 цифры)> ENTER
                         - <адрес (не более 200 символов)> ENTER
                         - <время встречи (к примеру, 18.09.2025 23:09)>""",
-                currentForm != null ? currentForm.receiverTag : "N/A",
-                currentForm != null ? currentForm.address : "N/A",
-                currentForm != null ? currentForm.datingTime : "N/A"));
+                currentForm != null ? currentForm.receiverTag : "Не указано",
+                currentForm != null ? "XXXX" : "Не указано",
+                currentForm != null ? currentForm.address : "Не указано",
+                currentForm != null ? currentForm.datingTime : "Не указано",
+                currentForm != null ? currentForm.activationTime : "Не указано",
+                isAllFieldFilled ? "" : "Учтите, что анкета отправится только в том случае, когда все поля анкеты" +
+                        "заполнены. Время активации указывать не обязательно."
+                ));
     }
 }
